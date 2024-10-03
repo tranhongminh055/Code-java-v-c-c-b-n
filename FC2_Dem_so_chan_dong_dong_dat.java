67f3d0fc6d7534b4738e597b93a2abb7426d6bb3
tranhongminh055 <151485869+tranhongminh055@users.noreply.github.com>
2024-10-04 00:06:05 +0700

    Add files via upload

---
+++ FC2_Dem_so_chan_dong_dong_dat.java
@@ -0,0 +1,13 @@
+package minhtran;
+
+	public class FC2_Dem_so_chan_dong_dong_dat {
+		int []a;
+		void nhap() {}
+		void in() {}
+		void dcd(){}
+	public static void main(String[] args) {
+FC2_Dem_so_chan_dong_dong_dat m= new
+FC2_Dem_so_chan_dong_dong_dat();
+m.nhap(); m.in(); m.dcd();
+}
+}
